8d093094a401b675dcd41d5d8dabb4f9be24056b
Chetan Kini <ckini123@gmail.com>
2021-02-19 10:52:04 -0500

    refactor(UI): Split up fuzzy search bar/results
    Due to FuzzySearchBar being 400ish lines, I decided it would be best to
    split up the actual search bar and the derived results into 2 separate
    files.
    
    The idea of passing props from the parent to the child seems a bit
    cumbersome here since there is a decent amount of shared state but I'm
    going through with it for now.
    
    Perhaps a future refactor is in store but as long as the functionality
    is as expected, that task will be put in the backlog.

--- client/src/components/FuzzySearchBar/FuzzySearchBar.tsx
+++ client/src/components/FuzzySearchBar/FuzzySearchBar.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import AppBar from '@material-ui/core/AppBar';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControl from '@material-ui/core/FormControl';
 import Fuse from 'fuse.js';
@@ -10,22 +11,10 @@ import ListItemText from '@material-ui/core/ListItemText';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
 import Toolbar from '@material-ui/core/Toolbar';
-import {
-	FormHelperText,
-	TextField,
-	Dialog,
-	Avatar,
-	ListItem,
-	List,
-	Switch,
-	Button,
-	DialogActions,
-	DialogTitle,
-	Typography,
-	DialogContent,
-} from '@material-ui/core';
+import { FormHelperText, TextField } from '@material-ui/core';
 import { RecurserGraph, RecurserNode } from '../../types/RecurserGraph';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { FuzzySearchResults } from '../FuzzySearchResults/FuzzySearchResults';
 
 const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
@@ -48,20 +37,6 @@ const useStyles = makeStyles((theme: Theme) =>
 		},
 		extendedIcon: {
 			marginRight: theme.spacing(1),
-		},
-		dialog: {
-			margin: 0,
-			padding: theme.spacing(2),
-		},
-		avatar: {
-			width: theme.spacing(7),
-			height: theme.spacing(7),
-		},
-		closeButton: {
-			position: 'absolute',
-			right: theme.spacing(1),
-			top: theme.spacing(1),
-			color: theme.palette.grey[500],
 		},
 	}),
 );
@@ -108,8 +83,11 @@ export const FuzzySearchBar: React.FC<Props> = props => {
 	const [selectedResults, setSelectedResults] = React.useState<
 		Set<Fuse.FuseResult<RecurserNode>>
 	>(new Set<Fuse.FuseResult<RecurserNode>>());
-	const [threshold, setThreshold] = React.useState<number>(0);
-	const [resultCap, setResultCap] = React.useState<number>(0);
+	const [
+		recurserSearchValue,
+		setRecurserSearchValue,
+	] = React.useState<RecurserNode | null>(props.currNode);
+	const [recurserInputValue, setRecurserInputValue] = React.useState('');
 
 	const fuse = React.useMemo(() => {
 		return new Fuse(props.graphData.nodes, {
@@ -120,17 +98,11 @@ export const FuzzySearchBar: React.FC<Props> = props => {
 
 	const handleDialogOpen = (event: React.KeyboardEvent) => {
 		if (event.keyCode === 13) {
-			setSearchResults(fuse.search(searchQuery));
+			let results = fuse.search(searchQuery);
+			setSearchResults(results);
+			setSelectedResults(new Set(results));
 			setOpenDialog(true);
 		}
-	};
-
-	const handleDialogClose = () => {
-		setSearchResults([]);
-		setSelectedResults(new Set<Fuse.FuseResult<RecurserNode>>());
-		setThreshold(0);
-		setResultCap(0);
-		setOpenDialog(false);
 	};
 
 	const handleCriteriaChange = (
@@ -143,171 +115,32 @@ export const FuzzySearchBar: React.FC<Props> = props => {
 		setSearchQuery(event.target.value);
 	};
 
-	const handlePageChange = (url: string | undefined) => {
-		return function() {
-			if (typeof url === 'string') {
-				window.open('http://' + url, '_blank');
-			}
-		};
-	};
-
-	const handleSwitchState = (result: Fuse.FuseResult<RecurserNode>) => {
-		return selectedResults.has(result);
-	};
-
-	const handleSwitchToggle = (result: Fuse.FuseResult<RecurserNode>) => {
-		return function() {
-			let newResultNodes = new Set<Fuse.FuseResult<RecurserNode>>(
-				selectedResults,
+	const handleRecurserSearchSubmit = (event: React.KeyboardEvent) => {
+		if (event.keyCode === 13 && recurserSearchValue) {
+			props.fgRef.current.zoom(8, 2000);
+			props.fgRef.current.centerAt(
+				recurserSearchValue.x,
+				recurserSearchValue.y,
+				1000,
 			);
-			if (selectedResults.has(result)) {
-				newResultNodes.delete(result);
-			} else {
-				newResultNodes.add(result);
-			}
-			setSelectedResults(newResultNodes);
-		};
-	};
-
-	const handleVisualizeButtonClick = () => {
-		let newGraphData = { ...props.graphData };
-		let selectedNodes = new Set<RecurserNode>();
-		selectedResults.forEach(res => selectedNodes.add(res.item));
-
-		for (let i = 0; i < newGraphData.nodes.length; i++) {
-			let currNode = newGraphData.nodes[i];
-			if (selectedNodes.has(currNode)) {
-				newGraphData.nodes[i].color = '#3dc06c';
-			} else {
-				newGraphData.nodes[i].color = '#000000';
-			}
 		}
-
-		props.setGraphData(newGraphData);
-		setOpenDialog(false);
-	};
-
-	const handleThresholdChange = (
-		event: React.ChangeEvent<{ value: string }>,
-	) => {
-		let conversion = parseInt(event.target.value);
-		if (!conversion || !(conversion >= 0 && conversion <= 100)) {
-			return; // TODO: Change up the error message and disable!
-		}
-		setThreshold(conversion);
-	};
-
-	const handleThresholdSubmit = (event: React.KeyboardEvent) => {
-		if (event.keyCode === 13) {
-			let newSelectedResults = new Set<Fuse.FuseResult<RecurserNode>>();
-			for (let i = 0; i < searchResults.length; i++) {
-				let resultScore = searchResults[i].score;
-				if (resultScore && Math.floor(100 * (1 - resultScore)) < threshold) {
-					break;
-				}
-				newSelectedResults.add(searchResults[i]);
-			}
-			setSelectedResults(newSelectedResults);
-		}
-	};
-
-	const handleResultCapChange = (
-		event: React.ChangeEvent<{ value: string }>,
-	) => {
-		let conversion = parseInt(event.target.value);
-		if (
-			!conversion ||
-			!(conversion >= 0 && conversion <= searchResults.length)
-		) {
-			return; // TODO: Change up the error message and disable!
-		}
-		setResultCap(conversion);
-	};
-
-	const handleResultCapSubmit = (event: React.KeyboardEvent) => {
-		if (event.keyCode === 13) {
-			let newSelectedResults = new Set<Fuse.FuseResult<RecurserNode>>();
-			for (let i = 0; i < resultCap; i++) {
-				newSelectedResults.add(searchResults[i]);
-			}
-			setSelectedResults(newSelectedResults);
-		}
-	};
-
-	const listSearchResults = () => {
-		let stringifyScore = (score: number) => {
-			let val = Math.floor(100 * (1 - score));
-			return <i> {val}% similarity</i>;
-		};
-
-		return searchResults.map(res => (
-			<ListItem divider={true} key={res.item.id}>
-				<Switch
-					checked={handleSwitchState(res)}
-					onClick={handleSwitchToggle(res)}
-				/>
-				<Avatar
-					className={classes.avatar}
-					src={res.item.imagePath}
-					onClick={handlePageChange(res.item.profilePath)}
-				/>
-				<Typography>
-					{res.item.name} ({res.item.batchShortName}) |{' '}
-					{res.score ? stringifyScore(res.score) : null}
-				</Typography>
-			</ListItem>
-		));
 	};
 
 	return (
 		<div className={classes.root}>
-			<Dialog
-				className={classes.dialog}
-				onClose={handleDialogClose}
-				open={openDialog}
-				fullWidth={true}
-				maxWidth={'sm'}
-			>
-				<DialogTitle id="customized-dialog-title">Search Results</DialogTitle>
-				<DialogContent>
-					<div>
-						You've currently selected <b>{selectedResults.size}</b> of{' '}
-						<b>{searchResults.length}</b> possible results.
-						<br />
-						If you'd like to change this, please filter or manually switch
-						connections below.
-						<br />
-					</div>
-					<FormControl className={classes.formControl}>
-						<TextField
-							onChange={handleThresholdChange}
-							onKeyDown={handleThresholdSubmit}
-							label="Set similarity threshold "
-						/>
-						<FormHelperText>
-							<i>Set minimum threshold (Enter to submit)</i>
-						</FormHelperText>
-					</FormControl>
-					<FormControl className={classes.formControl}>
-						<TextField
-							onChange={handleResultCapChange}
-							onKeyDown={handleResultCapSubmit}
-							label="Set result cap"
-						/>
-						<FormHelperText>
-							<i>Set number of results (Enter to submit)</i>
-						</FormHelperText>
-					</FormControl>
-					<List>
-						{searchResults.length > 0
-							? listSearchResults()
-							: 'No results found; please check your input criteria and query.'}
-					</List>
-				</DialogContent>
-				<DialogActions>
-					<Button onClick={handleVisualizeButtonClick}>Visualize</Button>
-				</DialogActions>
-			</Dialog>
+			<FuzzySearchResults
+				graphData={props.graphData}
+				setGraphData={props.setGraphData}
+				currNode={props.currNode}
+				setCurrNode={props.setCurrNode}
+				fgRef={props.fgRef}
+				searchResults={searchResults}
+				setSearchResults={setSearchResults}
+				selectedResults={selectedResults}
+				setSelectedResults={setSelectedResults}
+				openDialog={openDialog}
+				setOpenDialog={setOpenDialog}
+			/>
 			<AppBar className={classes.appBar} position="static">
 				<Toolbar>
 					<IconButton
@@ -348,6 +181,30 @@ export const FuzzySearchBar: React.FC<Props> = props => {
 							</FormHelperText>
 						</FormControl>
 					</IconButton>
+					<Autocomplete
+						value={recurserSearchValue}
+						onChange={(_event: any, newValue: RecurserNode | null) => {
+							setRecurserSearchValue(newValue);
+						}}
+						inputValue={recurserInputValue}
+						onInputChange={(_event, newInputValue) => {
+							setRecurserInputValue(newInputValue);
+						}}
+						id="controllable-states-demo"
+						options={props.graphData.nodes}
+						getOptionLabel={option =>
+							`${option.name} (${option.batchShortName})`
+						}
+						style={{ width: 300 }}
+						renderInput={params => (
+							<TextField
+								{...params}
+								label="Search by Recurser name"
+								variant="outlined"
+							/>
+						)}
+						onKeyDown={handleRecurserSearchSubmit}
+					/>
 				</Toolbar>
 			</AppBar>
 		</div>
