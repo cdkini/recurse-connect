ffb8642cbf45f44f5f0fd9cc87a597f27a23b66d
Chetan Kini <ckini123@gmail.com>
2021-04-14 17:16:46 -0400

    refactor(middleware): Move json.go to headers.go

--- server/middleware/headers.go
+++ server/middleware/headers.go
@@ -7,9 +7,10 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func SetContentType(r *mux.Router, env *environment.Env) mux.MiddlewareFunc {
+func SetDefaultHeaders(r *mux.Router, env *environment.Env) mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			r.Header.Set("Content-Type", "application/json")
 			w.Header().Set("Content-Type", "application/json")
 			next.ServeHTTP(w, r)
 		})
