07a99376ea901dea5b5d29a5283e090f5c2cf2a2
Chetan Kini <ckini123@gmail.com>
2021-04-22 08:46:29 -0400

    feat(main): Move main logic to server methods

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-delve/delve v1.6.0 // indirect
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/gorilla/mux v1.8.0
@@ -13,7 +14,20 @@ require (
 	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/lib/pq v1.10.0
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-runewidth v0.0.12 // indirect
+	github.com/peterh/liner v1.2.1 // indirect
+	github.com/pkg/errors v0.9.1
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/russross/blackfriday v1.6.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/spf13/cobra v1.1.3 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
+	go.starlark.net v0.0.0-20210416142453-1607a96e3d72 // indirect
+	golang.org/x/arch v0.0.0-20210405154355-08b684f594a5 // indirect
+	golang.org/x/sys v0.0.0-20210415045647-66c3f260301c // indirect
+	golang.org/x/tools/gopls v0.6.10 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
--- main.go
+++ main.go
@@ -1,26 +1,24 @@
 package main
 
 import (
-	"context"
 	"database/sql"
 	"fmt"
 	"io"
 	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"time"
 
-	"github.com/cdkini/recurse-connect/server/environment"
-	"github.com/cdkini/recurse-connect/server/router"
+	"os"
+
+	"github.com/cdkini/recurse-connect/server"
+	"github.com/cdkini/recurse-connect/server/database"
 	"github.com/gorilla/sessions"
 	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	// Load env variables into scope for DB
-	err := godotenv.Load(".env")
-	if err != nil {
+	// Load env variables into scope
+	if err := godotenv.Load(".env"); err != nil {
 		log.Fatalf("Could not read environment variables from file: %s", err)
 	}
 
@@ -28,16 +26,17 @@ func main() {
 	connectionString := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
 		os.Getenv("APP_DB_USERNAME"), os.Getenv("APP_DB_PASSWORD"), os.Getenv("APP_DB_NAME"))
 
-	db, err := sql.Open("postgres", connectionString)
+	conn, err := sql.Open("postgres", connectionString)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
+	defer conn.Close()
 
-	err = db.Ping()
-	if err != nil {
+	if err = conn.Ping(); err != nil {
 		log.Fatalf("Could not obtain connection to database: %v", err)
 	}
+
+	postgres := database.NewPostgresDB(conn)
 
 	// Configure logger and have it both print to STDOUT and write to a log file
 	logger := &log.Logger{}
@@ -51,38 +50,12 @@ func main() {
 	mw := io.MultiWriter(os.Stdout, f)
 	logger.SetOutput(mw)
 
-	// Initialize environment, router, and server
+	// Initialize mux router and server
 	store := sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
-	env := environment.NewEnv(db, logger, store)
-	r := router.Initialize(env)
+	server := server.NewServer(postgres, logger, store)
+	r := server.Route()
 	port := os.Getenv("APP_PORT")
 
-	srv := &http.Server{
-		Handler: r,
-		Addr:    fmt.Sprintf("127.0.0.1%s", port),
-		// Good practice: enforce timeouts for servers you create!
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
-	}
-
-	// Start server on desired port
-	fmt.Printf("Running server on http://127.0.0.1%s\n", port)
-	go func() {
-		err := srv.ListenAndServe()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
-
-	// Trap sigterm or interrupt to gracefully shutdown server
-	sigChan := make(chan os.Signal)
-	signal.Notify(sigChan, os.Interrupt)
-	signal.Notify(sigChan, os.Kill)
-
-	sig := <-sigChan // Blocking
-	log.Println("Received termination, gracefully shut down", sig)
-
-	// Wait a maximum of 30 seconds while cleaning up
-	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
-	srv.Shutdown(ctx)
+	// Run application!
+	server.ListenAndServe(r, port)
 }
